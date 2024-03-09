bd8fde234c70d4b7cfc4b6162703ebe560da9d56
johnkaa <g.johnka@gmail.com>
2024-03-09 03:53:51 +0200

    added atomic design

---
+++ src/components/atoms/AtomIcons.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg class="atom-icons">
+    <!--    <symbol id="icon-" viewBox=""></symbol>-->
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.atom-icons {
+  display: none;
+}
+</style>
---
+++ src/components/atoms/AtomLogo.vue
@@ -0,0 +1,5 @@
+<template>
+  <router-link to="/">
+    <img src="/images/logo.svg" alt="" width="171" height="33" />
+  </router-link>
+</template>
